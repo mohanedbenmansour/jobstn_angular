1d4893cf4f075289c065b706e19a7c17940520c1
mohaned benmansour <mohaned.benmansour@esprit.tn>
2020-07-19 16:30:17 +0100

    adding the admin dashboard

--- client/src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ client/src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { PostService } from '../../services/post.service';
+import { Post } from '../../shared/post';
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.scss']
+  styleUrls: ['./admin-dashboard.component.scss'],
 })
 export class AdminDashboardComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private postService: PostService,
+    private userService: UserService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  logout() {
+    this.userService.deleteEmail();
+    this.userService.deleteRole();
+    this.userService.deleteToken();
+    this.router.navigateByUrl('/signin');
   }
-
 }
